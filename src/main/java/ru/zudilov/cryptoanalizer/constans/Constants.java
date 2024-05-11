2d7b2db24eb08deefd03589cba03640c60b9014b
cyberbob99 <ab-zudilov@yandex.ru>
2024-05-11 17:36:29 +0300

    Алфавит сделал public

--- src/main/java/ru/zudilov/cryptoanalizer/constans/Constants.java
+++ src/main/java/ru/zudilov/cryptoanalizer/constans/Constants.java
@@ -8,7 +8,7 @@ import java.util.List;
 public class Constants {
     private Constants(){  }
     //Алфавит для шифрования
-    protected static final List<Character> ALPHABET = new ArrayList<>(Arrays.asList('А', 'Б', 'В', 'Г', 'Д', 'Е', 'Ё', 'Ж', 'З',
+    public static final List<Character> ALPHABET = new ArrayList<>(Arrays.asList('А', 'Б', 'В', 'Г', 'Д', 'Е', 'Ё', 'Ж', 'З',
             'И', 'Й', 'К', 'Л', 'М', 'Н', 'О', 'П', 'Р', 'С', 'Т', 'У', 'Ф', 'Х', 'Ц', 'Ч', 'Ш', 'Щ',
             'Ъ', 'Ы', 'Ь', 'Э', 'Ю', 'Я', 'а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з',
             'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ',
